93b9117cb1221563db2fb75fe14cf71df23dd31e
ajos3ph <andre.cr.vg@hotmail.com>
2019-06-03 11:09:31 -0300

    Add files via upload

---
+++ Lista04_Ex02/Lista04_Ex02.java
@@ -0,0 +1,25 @@
+package Lista04_Ex02;
+import java.util.Scanner;
+
+public class Lista04_Ex02 {
+    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o X do retangulo: ");
+        int x = scanner.nextInt();
+
+        System.out.println("Digite o Y do retangulo: ");
+        int y = scanner.nextInt();
+
+        Retangulo retangulo = new Retangulo(x, y);
+        System.out.println("Area: " + retangulo.getArea());
+
+        Retangulo retangulo1 = new Retangulo();
+        retangulo1.setX(x);
+        retangulo1.setY(y);
+
+        System.out.println("Area: " + retangulo1.getArea());
+    }  
+}
